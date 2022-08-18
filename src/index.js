5432a1ec33cd5d89b126333c17cd64ba57063ad9
simondt <ditcsim@gmail.com>
2022-08-18 20:37:45 -0300

    NavBar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Nav from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Nav />
   </React.StrictMode>
 );
 
